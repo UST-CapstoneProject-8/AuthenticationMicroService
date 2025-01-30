90715502a531dedf1f1898838ad7213f94a9a726
Sanjug16 <135541846+Sanjug16@users.noreply.github.com>
2025-01-30 12:34:55 +0530

    cross origin policy updated.

--- src/main/java/com/project/AuthenticationMicroservice/controller/AuthController.java
+++ src/main/java/com/project/AuthenticationMicroservice/controller/AuthController.java
@@ -14,6 +14,7 @@ import java.util.stream.Collectors;
 
 @RestController
 @RequestMapping("/api/auth")
+@CrossOrigin(origins = "http://localhost:5173")
 public class AuthController {
 
     @Autowired
